fea1664dcf104c165f56b8a21869d30f23969691
Nathan Kau <nathankau@gmail.com>
2025-01-29 15:28:34 -0800

    add option for mesh terrain. reset envs with high velwq

--- simulate_pupper.py
+++ simulate_pupper.py
@@ -4,18 +4,22 @@ import math
 import genesis as gs
 import torch
 import argparse
+from collections import deque
 
 parser = argparse.ArgumentParser(description="Simulate Pupper")
 parser.add_argument("--vis", action="store_true", help="Enable visualization")
 parser.add_argument("--use_terrain", action="store_true", help="Use terrain")
 parser.add_argument("--n_envs", type=int, default=1000, help="Number of environments")
 parser.add_argument("--go2", action="store_true", help="Use Go2")
+parser.add_argument("--mesh", action="store_true", help="Use mesh")
 args = parser.parse_args()
 
 VIS = args.vis
 USE_TERRAIN = args.use_terrain
 N_ENVS = args.n_envs
-DT = 0.004
+DT = 0.02
+SUBSTEPS = 5
+RESET_STEPS = 50
 
 
 def small_quaternions(batch_size=8, max_angle_deg=30, max_yaw_deg=180):
@@ -87,14 +91,16 @@ scene = gs.Scene(
     ),
     sim_options=gs.options.SimOptions(
         dt=DT,
-        substeps=1,
+        substeps=SUBSTEPS,
     ),
     rigid_options=gs.options.RigidOptions(
+        integrator=gs.integrator.implicitfast,
         dt=DT,
         constraint_solver=gs.constraint_solver.Newton,
         enable_collision=True,
         enable_self_collision=True,
         enable_joint_limit=True,
+        # use_contact_island=True,
     ),
     show_viewer=VIS,
 )
@@ -108,13 +114,15 @@ if args.go2:
             merge_fixed_links=True,
             pos=np.array([0, 0, 0.8]),
             quat=np.array([1.0, 0, 0, 0]),
-        )
+        ),
+        vis_mode="collision",
     )
 else:
     pupper = scene.add_entity(
         gs.morphs.MJCF(
             file="/home/nathankau/pupperv3/ros2_ws/src/pupper_v3_description/description/mujoco_xml/pupper_v3_complete.position.full_collision.xml",
         ),
+        vis_mode="collision",
     )
 terrain_cfg = {
     "vertical_scale": 0.10,
@@ -122,24 +130,27 @@ terrain_cfg = {
     "n_subterrains": (3, 3),
     "subterrain_size": (8, 8),
     "subterrain_types": [
-        ["flat_terrain", "random_uniform_terrain", "fractal_terrain"],
-        ["pyramid_sloped_terrain", "stepping_stones_terrain", "sloped_terrain"],
+        # ["flat_terrain", "random_uniform_terrain", "fractal_terrain"],
+        # ["pyramid_sloped_terrain", "stepping_stones_terrain", "sloped_terrain"],
+        # ["wave_terrain", "stairs_terrain", "pyramid_stairs_terrain"],
+        # [
+        #     "flat_terrain",
+        #     "flat_terrain",
+        #     "flat_terrain",
+        # ],
+        # [
+        #     "flat_terrain",
+        #     "flat_terrain",
+        #     "flat_terrain",
+        # ],
+        # [
+        #     "flat_terrain",
+        #     "flat_terrain",
+        #     "flat_terrain",
+        # ],
+        ["flat_terrain", "random_uniform_terrain", "random_uniform_terrain"],
+        ["pyramid_sloped_terrain", "stepping_stones_terrain", "pyramid_stairs_terrain"],
         ["wave_terrain", "stairs_terrain", "pyramid_stairs_terrain"],
-        # [
-        #     "flat_terrain",
-        #     "flat_terrain",
-        #     "flat_terrain",
-        # ],
-        # [
-        #     "flat_terrain",
-        #     "flat_terrain",
-        #     "flat_terrain",
-        # ],
-        # [
-        #     "flat_terrain",
-        #     "flat_terrain",
-        #     "flat_terrain",
-        # ],
     ],
 }
 
@@ -153,13 +164,37 @@ if USE_TERRAIN:
         subterrain_types=terrain_cfg["subterrain_types"],
     )
 
-    terrain = scene.add_entity(terrain_morph)
+    terrain = scene.add_entity(
+        terrain_morph,
+        vis_mode="collision",
+    )
+elif args.mesh:
+    mesh_terrain = gs.morphs.Mesh(
+        file="/home/nathankau/Downloads/mesh.obj",
+        pos=(0, 0, -1),
+        scale=(1, 1, 1),
+        convexify=False,
+        decompose_nonconvex=True,
+        fixed=True,
+        coacd_options=gs.options.CoacdOptions(
+            threshold=0.01, preprocess_resolution=100
+        ),
+    )
+    scene.add_entity(
+        mesh_terrain,
+        vis_mode="collision",
+    )
 else:
     plane = scene.add_entity(
         gs.morphs.Plane(),
     )
-ball = scene.add_entity(gs.morphs.Sphere(radius=0.1, pos=(-0.4, 7.44, 0.42)))
-ball2 = scene.add_entity(gs.morphs.Sphere(radius=0.1, pos=(-0.4, 7.44, 1.5)))
+# ball = scene.add_entity(gs.morphs.Sphere(radius=0.1, pos=(-0.4, 7.44, 0.42)))
+# ball2 = scene.add_entity(gs.morphs.Sphere(radius=0.1, pos=(-0.4, 7.44, 1.5)))
+
+
+# import ipdb
+
+# ipdb.set_trace()
 
 ########################## build ##########################
 scene.build(n_envs=N_ENVS)
@@ -218,27 +253,41 @@ pupper.set_dofs_force_range(
     dofs_idx_local=dofs_idx,
 )
 
+
+def reset_envs(env_idxs):
+    pos = (torch.rand((len(env_idxs), 3)) * 2 - 1) * 10.0
+    pos[:, 2] = 2.0
+    pupper.set_pos(
+        pos,
+        zero_velocity=True,
+        envs_idx=env_idxs,
+    )
+
+    # set random quaternion orientation
+    # generate random quatenrion
+    q = small_quaternions(batch_size=len(env_idxs), max_angle_deg=90)
+    pupper.set_quat(
+        q,
+        zero_velocity=True,
+        envs_idx=env_idxs,
+    )
+
+    pupper.set_dofs_position(
+        position=torch.zeros(len(env_idxs), 12),
+        dofs_idx_local=dofs_idx,
+        zero_velocity=True,
+        envs_idx=env_idxs,
+    )
+
+
 # PD control
 i = 0
-pos_history = []
+pos_history = deque(maxlen=50)
+vel_history = deque(maxlen=50)
 while True:
     print("step:", i, "total steps: ", i * N_ENVS)  # , pupper.get_qpos())
-    if i % 50 == 0:
-        pos = (torch.rand((N_ENVS, 3)) * 2 - 1) * 10.0
-        pos[:, 2] = 2.0
-        pupper.set_pos(
-            pos,
-            zero_velocity=True,
-            envs_idx=torch.arange(N_ENVS),
-        )
-        # set random quaternion orientation
-        # generate random quatenrion
-        q = small_quaternions(batch_size=N_ENVS, max_angle_deg=90)
-        pupper.set_quat(
-            q,
-            zero_velocity=True,
-            envs_idx=torch.arange(N_ENVS),
-        )
+    if i % RESET_STEPS == 0:
+        reset_envs(torch.arange(N_ENVS))
 
     pupper.control_dofs_position((torch.rand((N_ENVS, 12)) * 2 - 1) * 0.2, dofs_idx)
 
@@ -248,7 +297,17 @@ while True:
 
     # This is the actual force experienced by the dof
     # print("internal force:", pupper.get_dofs_force(dofs_idx))
+
+    high_vel = pupper.get_links_vel().norm(dim=-1) > 100  # shape (N_ENVS, 13)
+    if high_vel.any():
+        high_vel_idx = high_vel.any(dim=-1).nonzero().flatten()
+        gs.logger.warning(
+            f"velocity too high, resetting {high_vel_idx.detach().cpu().numpy()}"
+        )
+        reset_envs(high_vel_idx)
+
     pos_history.append(pupper.get_pos().clone().detach())
+    vel_history.append(pupper.get_links_vel().clone().detach())
     scene.step()
 
     isnans = torch.isnan(pupper.get_pos())
@@ -256,7 +315,11 @@ while True:
         import ipdb
 
         pos_tensor = torch.stack(pos_history)
-        print(pos_tensor[:, isnans.nonzero(), :])
+        print(pos_tensor[:, isnans.nonzero()[0, 0], :])
+
+        vel_tensor = torch.stack(vel_history)
+        print(vel_tensor[:, isnans.nonzero()[0, 0], 0])
+
         ipdb.set_trace()
     i += 1
     # import ipdb
